10d43c8c4c58fb39f9f5d1db5a99bee38f329440
Yamakasi <yamakasi2999@gmail.com>
2021-12-06 23:02:53 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
+
+export function Menu(){
+  return(
+    <div>
+      <a href='#'>Google</a>
+      <a href='#'>Yandex</a>
+    </div>
+  )
+}
+
